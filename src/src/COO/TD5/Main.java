275865fd8025038b2bc60e4c4c224b521875ff37
Antony Pinel <antony.pinel@etu.univ-amu.fr>
2021-10-21 16:45:02 +0200

    Oui

---
+++ src/src/COO/TD5/Main.java
@@ -0,0 +1,8 @@
+package COO.TD5;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        WeatherStation weatherStation = new WeatherStation();
+    }
+}

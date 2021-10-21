629a55202e7be8d399b2282d835576f2eea46f9e
Antony Pinel <antony.pinel@etu.univ-amu.fr>
2021-10-21 16:29:46 +0200

    Oui

--- src/src/COO/TD5/WeatherStation.java
+++ src/src/COO/TD5/WeatherStation.java
@@ -1,24 +1,44 @@
 package COO.TD5;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 
  */
 public class WeatherStation extends Subject {
+
+    private List<Sensor> mySensors;
+    private List<Observer> myObservers;
+
+
     public WeatherStation() {
+        this.myObservers = new ArrayList<Observer>();
+        this.mySensors = new ArrayList<Sensor>();
+
+        mySensors.add(new TemperatureSensor());
+        mySensors.add(new WindSensor());
+
+        while (true) {
+            System.out.println("Weather update : ");
+            System.out.println("Temp = " + );
+        }
     }
 
     @Override
     public void attacher(Observer o) {
-
+        myObservers.add(o);
     }
 
     @Override
     public void detacher(Observer o) {
-
+        myObservers.remove(o);
     }
 
     @Override
     public void notifier() {
+
     }
 
 }
